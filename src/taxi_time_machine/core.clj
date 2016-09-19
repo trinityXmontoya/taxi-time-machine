ae66ec06219691826d9e690ec5840b9a0c93888b
trinityXmontoya <trinityXmontoya@gmail.com>
2016-09-19 02:56:27 -0400

    cleanup

--- src/taxi_time_machine/core.clj
+++ src/taxi_time_machine/core.clj
@@ -7,34 +7,32 @@
             [ring.middleware.not-modified :refer [wrap-not-modified]]
             [clojure.java.io :as io]
             [cheshire.core :as json]
-            ; [clj-kafka.new.producer :as kf-producer]
-            ; [clj-kafka.admin :as kf-admin]
-            ; [kafka-clj.client :as kafka]
             [taxi-time-machine.geomesa :as geomesa]
             [clj-time.core :as t]
             [clj-time.coerce :as c]
             [clj-time.format :as f]
             [clojure.java.shell :as shell])
   (:import (org.locationtech.geomesa.utils.text WKTUtils$))
-  ; (:import [org.geotools.data DataStoreFinder])
   (:gen-class))
 
 (timbre/refer-timbre)
 
-; mvn install:install-file -Dfile=jaad-0.8.3.jar -DartifactId=jaad -Dversion=0.8.3 -DgroupId=jaad -Dpackaging=jar -DlocalRepositoryPath=maven_repository
-;
+; formatters
+(defn row->hash
+  [row]
+  (let [fields [:vendor-id :pickup-datetime :dropoff-datetime :passenger-count
+                :trip-dist :pickup-lng :pickup-lat :rate-code-id :store-and-fwd-flag
+                :dropoff-lng :dropoff-lat :payment-type :fare-amt :extra :mta-tax
+                :tip-amt :tolls-amt :total-amt]
+       res (zipmap fields row)]
+       (assoc res  :pickup-datetime (->datetime (res :pickup-datetime))
+                   :dropoff-datetime (->datetime (res :dropoff-datetime)))))
 
-(defn handler [request]
-  {:status 200
-   :headers {"Content-Type" "text/html"}
-   :body (slurp "resources/public/index.html")})
-
-  ;  (println DataStoreFinder)
-
+; date helpers
 (def custom-formatter (f/formatter "yyyy-MM-dd HH:mm:ss"))
 (defn ->datetime [str] (f/parse custom-formatter str))
 (defn ->java-date [^org.joda.time.DateTime datetime] (c/to-date datetime))
-
+(defn calc-trip-time [start end] (t/in-seconds (t/interval start end)))
 
 ; ; OSRM
 (defn calc-route
@@ -53,10 +51,6 @@
   [coord]
   (.read WKTUtils$/MODULE$ (str "POINT(" (clojure.string/join " " coord) ")")))
 
-(defn calc-trip-time
-  [start end]
-  (t/in-seconds (t/interval start end)))
-
 (defn calc-stops
   [trip]
   (let [route (calc-route trip)
@@ -69,34 +63,6 @@
                           datetime (c/to-date (t/plus start (t/seconds (* secs-per-path i))))]
                       {:geom point :dtg datetime})) coords)))
 
-(defn row->hash
-  [row]
-  (let [fields [:vendor-id :pickup-datetime :dropoff-datetime :passenger-count
-                :trip-dist :pickup-lng :pickup-lat :rate-code-id :store-and-fwd-flag
-                :dropoff-lng :dropoff-lat :payment-type :fare-amt :extra :mta-tax
-                :tip-amt :tolls-amt :total-amt]
-       res (zipmap fields row)]
-       (assoc res :pickup-datetime (->datetime (res :pickup-datetime))
-                   :dropoff-datetime (->datetime (res :dropoff-datetime)))))
-
-;
-; ; KAFKA
-;
-; (def producer-conf (kf-producer/producer {"bootstrap.servers" "localhost:9092"}
-; (kf-producer/byte-array-serializer) (kf-producer/byte-array-serializer)))
-; (def producer-conf (kafka/create-connector [{:host "localhost" :port 9092}] {:flush-on-write true}))
-; ;
-;
-; (defn create-topic
-;   [name]
-;   (let [zk (kf-admin/zk-client "127.0.0.1:2181")]
-;   (kf-admin/create-topic zk name)))
-;
-; (defn send-to-kafka
-;   [msg]
-;   (kafka/send-msg producer-conf "butt" (.getBytes (json/encode msg))))
-;   @(kf-producer/send producer-conf (kf-producer/record "butt" (.getBytes (json/encode msg)))))
-;
 ; (defn run-osrm-server
 ;   []
 ;   (info "Running osrm server")
@@ -110,30 +76,20 @@
 ;   []
 ;   (println "kafka" (:out (shell/sh "start-kafka"))))
 
+; (defn create-topic
+;   [name]
+;   (let [zk (kf-admin/zk-client "127.0.0.1:2181")]
+;   (kf-admin/create-topic zk name)))
+
 (defn -main
   []
-  (println "im alive")
   (let [row ["1","2015-01-01 00:20:41","2015-01-01 00:27:07","1","1.20","-73.981498718261719","40.771186828613281","1","N","-73.972816467285156","40.782432556152344","2","7","0.5","0.5","0","0","8.3"]
-
         trip (row->hash row)
-
         stops (calc-stops trip)
         first-stop (first stops)
-
         processed-trip (assoc (dissoc trip :pickup-datetime :dropoff-datetime :pickup-lat
                                     :pickup-lng :dropoff-lat :dropoff-lng) :dtg (first-stop :dtg)
-                                                                           :geom (first-stop :geom))
-
-        ; res (merge row-as-hash {:stops stops})
-        ]
-        ; (println (drop 1 stops) processed-trip)
-    (geomesa/write-trip->kafka processed-trip (drop 1 stops))
-    ; (geomesa/replay)
-      ; (send-to-kafka res)
+                                                                           :geom (first-stop :geom))]
+    ; (geomesa/write-trip->kafka processed-trip (drop 1 stops))
+    (geomesa/replay)
       ))
-
-; (defn -main
-;   [& args]
-;   (let [port (Integer/parseInt (or (env :port) "8080"))]
-;     (info "Server running on port" port)
-;     (run-server handler {:port port})))
--- src/taxi_time_machine/geomesa.clj
+++ src/taxi_time_machine/geomesa.clj
@@ -52,17 +52,9 @@
 (defn build-simple-feature
   "build SimpleFeature"
   [^SimpleFeatureType sft obj id]
-  (let [builder (SimpleFeatureBuilder. sft)
-        ; pickup-coords (.read WKTUtils$/MODULE$ (str "POINT(" (:pickup-lat obj) " " (:pickup-lng obj) ")"))
-        ; dropoff-coords (.read WKTUtils$/MODULE$ (str "POINT(" (:pickup-lat obj) " " (:pickup-lng obj) ")"))
-        ; geom (obj :path)
-        ; obj (dissoc obj :pickup-lat :pickup-lng :dropoff-lat :dropoff-lng :path)
-        ]
+  (let [builder (SimpleFeatureBuilder. sft)]
       (doseq [attr obj]
         (.set builder (name (key attr)) (val attr)))
-      ; (.set builder "pickup-coords" pickup-coords)
-      ; (.set builder "dropoff-coords" dropoff-coords)
-      ; (.set builder "geom" geom)
     (.buildFeature builder id)))
 
 (defn add-simple-feature
@@ -90,29 +82,21 @@
 
 
 
-
+; conf
 
 (def ds-conf {"brokers" "localhost:9092"
                "zookeepers" "localhost:2181"
                "zkPath" "/geomesa/ds/kafka"
                "automated" "automated"})
-
 (def zk-path (or (ds-conf "zkPath") "/geomesa/ds/kafka"))
-
 (def sft-name "KafkaQuickStartClojureTest12")
 (def producer-ds (DataStoreFinder/getDataStore (merge ds-conf {"isProducer" true})))
 (def consumer-ds (DataStoreFinder/getDataStore (merge ds-conf {"isProducer" false})))
 (def sft (SimpleFeatureTypes/createType sft-name trip-schema))
 (def prepped-output-sft (KafkaDataStoreHelper/createStreamingSFT sft zk-path))
-
 (if (not (.contains (Arrays/asList (.getTypeNames producer-ds)) sft-name))
  (.createSchema producer-ds prepped-output-sft))
-
-
-
 (def producer-fs (.getFeatureSource producer-ds sft-name))
-
-
 
 (defn write-trip->kafka
   [trip points]
@@ -128,13 +112,8 @@
               (let [copy (.buildFeature builder id)]
                 (.add feature-collection copy)))) points)
     (.addFeatures producer-fs feature-collection)
-    (.clear feature-collection)
+    (.clear feature-collection)))
 
-    ))
-
-
-; "2015-01-01T00:19:41.000-00:00","2015-01-01T00:28:07.000-00:00"
-; 2014-12-31T19:19:41
 (defn replay
   []
   (let [replay-begin (.toInstant (DateTime. #inst "2014-12-31T19:12:41"))
@@ -158,115 +137,3 @@
            (.close feature-iterator)
            (print-feature feature1)
            (print-feature feature2)))))
-
-
-
-  (defn -main
-    ; [^String[] args]
-    []
-    (let [ds-conf {"brokers" "localhost:9092"
-                   "zookeepers" "localhost:2181"
-                   "zkPath" "/geomesa/ds/kafka"
-                   "automated" "automated"}
-          producer-ds (DataStoreFinder/getDataStore (merge ds-conf {"isProducer" true}))
-          consumer-ds (DataStoreFinder/getDataStore (merge ds-conf {"isProducer" false}))]
-
-      ; verify that we got back our KafkaDataStore objects properly
-      (if (nil? producer-ds)
-        (throw "Null producer KafkaDataStore"))
-      (if (nil? consumer-ds)
-        (throw "Null consumer KafkaDataStore"))
-
-      ; create the schema which creates a topic in Kafka
-      ; (only needs to be done once)
-      (let [sft-name "KafkaQuickStartClojureTest10"
-            sft-schema trip-schema
-            sft (SimpleFeatureTypes/createType sft-name sft-schema)
-            ; set zkPath to default if not specified
-            zk-path (or (ds-conf "zkPath") "/geomesa/ds/kafka")
-            prepped-output-sft (KafkaDataStoreHelper/createStreamingSFT sft zk-path)
-            ; only create the schema if it hasn't been created already
-            x (if (not (.contains (Arrays/asList (.getTypeNames producer-ds)) sft-name)) (.createSchema producer-ds prepped-output-sft))
-            ; the live consumer must be created before the producer writes features
-            ; in order to read streaming data.
-            ; i.e. the live consumer will only read data written after its instantiation
-            consumer-fs (.getFeatureSource consumer-ds sft-name)
-            producer-fs (.getFeatureSource producer-ds sft-name)
-            ; samples [(tc/calc-path {:tolls-amt "0", :pickup-lng "-73.981498718261719", :mta-tax "0.5", :store-and-fwd-flag "N", :extra "0.5", :dropoff-lat "40.782432556152344", :rate-code-id "1", :trip-dist "1.20", :pickup-lat "40.771186828613281", :dropoff-datetime #inst "2015-01-01T00:27:07.000-00:00", :passenger-count "1", :tip-amt "0", :dropoff-lng "-73.972816467285156", :fare-amt "7", :payment-type "2", :total-amt "8.3", :pickup-datetime #inst "2015-01-01T00:20:41.000-00:00", :vendor-id "1"}) (tc/calc-path {:tolls-amt "0.3", :pickup-lng "-74.981498718261719", :mta-tax "0.5", :store-and-fwd-flag "N", :extra "0.5", :dropoff-lat "40.782432556152344", :rate-code-id "1", :trip-dist "1.20", :pickup-lat "40.771186828613281", :dropoff-datetime #inst "2015-01-02T00:27:07.000-00:00", :passenger-count "1", :tip-amt "0", :dropoff-lng "-74.972816467285156", :fare-amt "7", :payment-type "2", :total-amt "8.3", :pickup-datetime #inst "2015-01-02T00:20:41.000-00:00", :vendor-id "1"})]
-            samples []
-            ]
-      ; creates and adds SimpleFeatures to the producer every 1/5th of a second
-      (println "Writing features to Kafka... refresh GeoServer layer preview to see changes")
-
-      (doseq [sample samples]
-        (add-simple-feature sft producer-fs sample)
-        )
-      ; (mapv #(add-simple-feature sft producer-fs %) samples)
-
-
-
-
-
-      ; (let [replay-start (Instant.)]
-      ;   (add-simple-features sft producer-fs)
-      ;   (let [replay-end (Instant.)]
-      ;
-      ;   ; read from Kafka after writing all the features.
-      ;   ; LIVE CONSUMER - will obtain the current state of SimpleFeatures
-      ;   (println "\nConsuming with the live consumer...")
-      ;   (let [feature-collection (.getFeatures consumer-fs)]
-      ;     (println (str (.size feature-collection) " features were written to Kafka"))
-      ;
-      ;     (add-delete-new-feature sft producer-fs)
-
-
-          ; read from Kafka after writing all the features.
-          ; LIVE CONSUMER - will obtain the current state of SimpleFeatures
-          (println "\nConsuming with the live consumer...")
-          (let [feature-collection (.getFeatures consumer-fs)]
-           (println "imthecollection" feature-collection)
-            (println (str (.size feature-collection) " features were written to Kafka"))
-
-          ; the state of the two SimpleFeatures is real time here
-          (println "Here are the two SimpleFeatures that were obtained with the live consumer:")
-          (let [feature-iterator (.features feature-collection)
-                feature1 (.next feature-iterator)]
-                (.close feature-iterator)
-                (print-feature feature1)
-
-
-            ;
-            ; REPLAY CONSUMER - will obtain the state of SimpleFeatures at any specified time
-            ; Replay consumer requires a ReplayConfig which takes a time range and a
-            ; duration of time to process
-            (println "\nConsuming with the replay consumer...")
-            (let [replay-end (.toInstant (DateTime. #inst "2015-01-03T00:20:41.000-00:00"))
-                  replay-begin (.toInstant (DateTime. #inst "2014-12-31T00:20:41.000-00:00"))
-                  read-behind (Duration. 1000); 1 second readBehind
-                  rc (ReplayConfig. replay-begin replay-end read-behind)
-                  replay-sft (KafkaDataStoreHelper/createReplaySFT prepped-output-sft rc)]
-              (.createSchema producer-ds replay-sft)
-              (let [replay-consumer-fs (.getFeatureSource consumer-ds (.getName replay-sft))
-                    ; querying for the state of SimpleFeatures approximately 5 seconds before the replay-end.
-                    ; the ReplayKafkaConsumerFeatureSource will build the state of SimpleFeatures
-                    ; by processing all of the messages that were sent in between queryTime-readBehind and queryTime.
-                    ; only the messages in between replay-start and replay-end are cached.
-                    query-time (.minus replay-end 5000)
-                    feature-collection (.getFeatures replay-consumer-fs (ReplayTimeHelper/toFilter query-time))]
-
-
-                    )))))))
-
-            ;     (println (str (.size feature-collection) " features were written to Kafka"))
-            ;     (println "Here are the two SimpleFeatures that were obtained with the replay consumer:")
-            ;
-            ;     (let [feature-iterator (.features feature-collection)
-            ;           feature1 (.next feature-iterator)
-            ;           feature2 (.next feature-iterator)]
-            ;           (.close feature-iterator)
-            ;           (print-feature feature1)
-            ;           (print-feature feature2)
-            ;
-            ;       (if (not (nil? (System/getProperty "clear")))
-            ;         (.removeFeatures producer-fs (.INCLUDE Filter)))
-            ;       (System/exit 0))))))))))))
